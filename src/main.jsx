09a9e0bc9bd22e3171fc16a5f8ae38e5a4042e93
gkell87 <g_kell87@yahoo.com>
2025-09-01 16:52:01 -0400

    test

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
-
-<Provider store={store}>
-  <App />
-</Provider>
-
-
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <App />
+    </Provider>
+  </React.StrictMode>,
+)
